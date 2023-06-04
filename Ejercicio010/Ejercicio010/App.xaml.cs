e6dc22ac8bb41356e973464fd80de352a031db24
Angela <angela.chancasanampa@tecsup.edu.pe>
2023-06-04 15:21:59 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio010/Ejercicio010/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Ejercicio010;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio010
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new ViewToViewDemo();
+            //MainPage = new BindingModeDemo();
+            //MainPage = new ListViewDemo();
+            MainPage = new PickerDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
